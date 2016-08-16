304f3c891bb8fdbb720923288364e27adcf46a9e
KyungWonLee <lkw1989@wemakeprice.com>
2016-08-16 12:07:51 +0900

    jenkins run in background

--- nginx-jenkins/run.sh
+++ nginx-jenkins/run.sh
@@ -1,5 +1,6 @@
 sudo docker pull jenkins
 sudo mkdir -p jenkins_home
-sudo docker run -p 8080:8080 -p 50000:50000 --name jenkins -v jenkins_home:/var/jenkins_home jenkins
+sudo docker run -p 8080:8080 -p 50000:50000 --name jenkins -v jenkins_home:/var/jenkins_home jenkins &
+sudo docker start jenkins
 sudo docker build -t nginx-jenkins .
 sudo docker run -p 80:80 --name web --link jenkins:jenkins -d nginx-jenkins
